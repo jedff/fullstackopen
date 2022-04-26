7d8770460687b26d7b3b9f9094f223f2269fba5c
Juan <jedfigueroa@gmail.com>
2022-04-26 16:48:19 -0700

    Add exercise 8.2: All books

--- part8/index.js
+++ part8/index.js
@@ -79,16 +79,25 @@ let books = [
 ]
 
 const typeDefs = gql`
+type Book {
+  title: String!
+  author: String!
+  published: Int!
+  genres: [String]!
+  id: ID!
+}
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
