8b7ad0592b0e68b9fc096273eee1ad613931e31d
Juan <jedfigueroa@gmail.com>
2022-04-26 18:17:06 -0700

    Add exercise 8.4: Books of an author

--- part8/index.js
+++ part8/index.js
@@ -95,7 +95,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book]
     allAuthors: [Author!]!
   }
 `
@@ -104,7 +104,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(b => b.author === args.author),
     allAuthors: () =>
       authors.map(author => {
         return {
